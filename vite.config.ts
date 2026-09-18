ff2de2d0be78e96ee27adc34166015d0b0905f4c
Ricky <ricintech@gmail.com>
2026-09-18 15:12:42 +0530

    config chnages done

--- vite.config.ts
+++ vite.config.ts
@@ -5,7 +5,7 @@ import {defineConfig} from 'vite';
 
 export default defineConfig(() => {
   return {
-    base: process.env.BASE_PATH || '/',
+    base: process.env.BASE_PATH || (process.env.GITHUB_REPOSITORY ? `/${process.env.GITHUB_REPOSITORY.split('/')[1]}/` : './'),
     plugins: [react(), tailwindcss()],
     resolve: {
       alias: {
